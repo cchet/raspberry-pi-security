c945959c0d7872c94838b726421cfca6848ad6ea
Thomas Herzog <herzog.thomas81@gmail.com>
2017-04-21 12:56:33 +0200

    - Added projects for sensor access and handling

---
+++ rpisec-java/rpisec-sensor/api/src/main/java/at/rpisec/sensor/api/DummyInterface.java
@@ -0,0 +1,10 @@
+package at.rpisec.sensor.api;
+
+/**
+ * Git does not allow empty directories
+ *
+ * @author Thomas Herzog <t.herzog@curecomp.com>
+ * @since 04/21/17
+ */
+public interface DummyInterface {
+}
---
+++ rpisec-java/rpisec-sensor/impl/src/main/java/at/rpisec/sensor/impl/DummyImpl.java
@@ -0,0 +1,12 @@
+package at.rpisec.sensor.impl;
+
+import at.rpisec.sensor.api.DummyInterface;
+
+/**
+ * Git does not allow empty directories
+ *
+ * @author Thomas Herzog <t.herzog@curecomp.com>
+ * @since 04/21/17
+ */
+public class DummyImpl implements DummyInterface {
+}
