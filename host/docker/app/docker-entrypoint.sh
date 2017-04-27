ce177dcebc2a04b1263a2f16f98dfd9db7dacf13
cchet <herzog.thomas81@gmail.com>
2017-04-27 13:22:22 +0000

    - Always copy jar to app-running.jar in docker-entrypoint.sh

--- host/docker/app/docker-entrypoint.sh
+++ host/docker/app/docker-entrypoint.sh
@@ -1,8 +1,5 @@
 #/bin/bash
 
-if [ ! -e ~/app-running.jar ];
-then
-	mv ~/app/*.jar ~/app/app-running.jar
-fi
+mv ~/app/*.jar ~/app/app-running.jar
 
 java -jar ${HOME}/app/app-running.jar --spring.profiles.active=prod --spring.config.location=file:${HOME}/conf/security-application.properties ${APP_JAVA_OPTS}
