e16ca54a7f37e7f16da06c8d10a230017009f0ab
cchet <herzog.thomas81@gmail.com>
2017-04-27 15:23:51 +0000

    - Got app with postgres running

--- host/docker/app/docker-entrypoint.sh
+++ host/docker/app/docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #/bin/bash
 
-mv ~/app/*.jar ~/app/app-running.jar
+if [ -e "${APP_VERSION}" ] || [ -z "${RPISEC_DB_NAME}" ] || [ -z "${RPISEC_DB_USER}" ] || [ -z "${RPISEC_DB_PASSWORD}" ] || [ -z "${POSTGRES_HOST}" ];
+then
+	echo "All env variables mut be given 'APP_VERSION, POSTGRES_HOST, RPISEC_DB_NAME, RPISEC_DB_USER, RPISEC_DB_PASSWORD'"
+	exit -1
+fi
+
+var APP_NAME=rpisec-app-${APP_VERSION}
+
+cp -f ~/app/${APP_NAME}.jar ~/app/app-running.jar
 
 java -jar ${HOME}/app/app-running.jar --spring.profiles.active=prod --spring.config.location=file:${HOME}/conf/security-application.properties ${APP_JAVA_OPTS}
