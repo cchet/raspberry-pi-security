b5e29d4aeae77424e0d1022fc59daaf652c80563
Thomas Herzog <herzog.thomas81@gmail.com>
2017-04-28 07:53:27 +0200

    - create branch for testing externalizing the configuration which shouldn't be packaged with the application

--- host/docker/app/docker-entrypoint.sh
+++ host/docker/app/docker-entrypoint.sh
@@ -10,4 +10,4 @@ APP_NAME=rpisec-app-${APP_VERSION}
 
 cp -f ~/app/${APP_NAME}.jar ~/app/app-running.jar
 
-java -jar ${HOME}/app/app-running.jar --spring.profiles.active=prod --spring.config.location=file:${HOME}/conf/security-application.properties ${APP_JAVA_OPTS}
+java -jar ${HOME}/app/app-running.jar --spring.profiles.active=prod ${APP_JAVA_OPTS}
