464d2ff11bcd9d2fc7a6ca3320d9b5387354f0d3
phiwu <emtpy>
2017-05-14 12:32:01 +0200

    - finished andorid test-app

--- rpisec-android/app/src/main/java/at/rpisec/client/RPISecFirebaseMessagingService.java
+++ rpisec-android/app/src/main/java/at/rpisec/client/RPISecFirebaseMessagingService.java
@@ -1,7 +1,5 @@
 package at.rpisec.client;
 
-import android.util.Log;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -15,23 +13,16 @@ public class RPISecFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // ...
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        System.out.println("[DEBUG] From: " + remoteMessage.getFrom());
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            System.out.println("[DEBUG] Message data payload: " + remoteMessage.getData());
         }
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            System.out.println("[DEBUG] Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
 }
