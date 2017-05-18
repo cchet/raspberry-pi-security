35324f586ef3d77cd965c532ce3454917e9ef1cf
Thomas Herzog <herzog.thomas81@gmail.com>
2017-05-18 09:16:06 +0200

    - started to modify existing docker infrastructure and doc

---
+++ host/docker/oauth/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+if [ -z "${APP_VERSION}" ];
+then
+	echo "environment variable 'APP_VERSION' must be set"
+	exit -1
+fi
+
+APP_NAME=rpisec-app-${APP_VERSION}
+
+cp -f ~/app/${APP_NAME}.jar ~/app/app-running.jar
+
+java -jar ${HOME}/app/app-running.jar --spring.profiles.active=prod --spring.config.location=file:${HOME}/conf/oauth-external-application.properties  ${APP_JAVA_OPTS}
