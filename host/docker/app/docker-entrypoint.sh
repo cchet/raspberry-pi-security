77b823d17dc88e44ca157703bf482f297a53dfd8
Thomas Herzog <herzog.thomas81@gmail.com>
2017-05-25 13:22:03 +0200

    - Modified docker files and docker-compose file - added env file for docker infrastructure - added gpio and movement sensor scheme images

--- host/docker/app/docker-entrypoint.sh
+++ host/docker/app/docker-entrypoint.sh
@@ -1,13 +1,11 @@
 #/bin/bash
 
-if [ -z "${APP_VERSION}" ];
+if [ -z "${RPISEC_VERSION}" ];
 then
 	echo "environment variable 'APP_VERSION' must be set"
 	exit -1
 fi
 
-APP_NAME=rpisec-app-${APP_VERSION}
+cp -f ~/app/rpisec-app-${RPISEC_VERSION}.jar ~/app/app-${RPISEC_VERSION}-running.jar
 
-cp -f ~/app/${APP_NAME}.jar ~/app/app-running.jar
-
-java -jar ${HOME}/app/app-running.jar --spring.profiles.active=prod --spring.config.location=file:${HOME}/conf/external-application.properties  ${APP_JAVA_OPTS}
+java -jar ${HOME}/app/app-${RPISEC_VERSION}-running.jar --spring.profiles.active=prod --spring.config.location=file:${HOME}/conf/app-external-application.properties ${APP_JAVA_OPTS}
