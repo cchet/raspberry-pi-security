5e36f744e0db6f414f77a259f23bfcb5192077e1
phiwu <emtpy>
2017-05-30 12:48:12 +0200

    - changed from jack & jill to retrolambda to avoid problems in :app:transformClassesWithPreJackPackagedLibrariesForDebug (can be revoked in android studio 2.4.x+)

--- rpisec-android/app/src/main/java/at/rpisec/client/LoginActivity.java
+++ rpisec-android/app/src/main/java/at/rpisec/client/LoginActivity.java
@@ -234,9 +234,6 @@ public class LoginActivity extends AppCompatActivity {
         protected String doInBackground(Void... params) {
 
             try {
-                OAuth2Parameters
-
-
                 ResponseEntity<String> registerResponse = getRpiSecRestTemplate().exchange(UriRegToken, HttpMethod.PUT, getHttpEntity(), String.class);
 
                 if (registerResponse.getStatusCode() == HttpStatus.OK) {
