9148d5c458093bcfad4f4051e7f34725cee24b62
phiwu <s1610454038@students.fh-hagenberg.at>
2017-06-08 13:33:21 +0200

    - added @JsonIgnoreProperties(ignoreUnknown = true)

--- rpisec-java/rpisec-shared/src/main/java/at/rpisec/server/shared/rest/model/FirebaseMessage.java
+++ rpisec-java/rpisec-shared/src/main/java/at/rpisec/server/shared/rest/model/FirebaseMessage.java
@@ -1,5 +1,6 @@
 package at.rpisec.server.shared.rest.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FirebaseMessage {
 
     private String to;
