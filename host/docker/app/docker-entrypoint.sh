77fe0fd0b3192997f94f7de8f4097078268e44cd
cchet <herzog.thomas81@gmail.com>
2017-06-17 14:30:26 +0000

    - New docker infrastructure running on pi

--- host/docker/app/docker-entrypoint.sh
+++ host/docker/app/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-exec sudo java ${APP_JAVA_OPTS} -Duser.home=${HOME} -jar ${HOME}/app/app-service.jar --spring.profiles.active=prod --spring.config.location=file:${HOME}/conf/app-external-application.properties
+exec sudo java ${APP_JAVA_OPTS} -Duser.home=${HOME} -jar ${HOME}/app/app-service.jar --spring.profiles.active=prod --spring.config.location=file:${HOME}/conf/app.properties
