32a98ffa6bd78c5186e0e7b09e7fb99c56e54eff
Thomas Herzog <herzog.thomas81@gmail.com>
2017-06-18 17:49:47 +0200

    - increased memory for services app and auth and added, fixed invalid directory and removed volume mappings for service in test compose file

---
+++ java/testsuite/client/src/test/java/at/rpisec/testsuite/client/test/rest/api/OautApiTest.java
@@ -0,0 +1,10 @@
+package at.rpisec.testsuite.client.test.rest.api;
+
+import at.rpisec.testsuite.client.test.api.BaseIntegrationTest;
+
+/**
+ * @author Thomas Herzog <herzog.thomas81@gmail.com>
+ * @since 06/09/17
+ */
+public class OautApiTest extends BaseIntegrationTest {
+}
