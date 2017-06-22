d863838b578675ed4ddee1532f99f1702482d7df
Thomas Herzog <herzog.thomas81@gmail.com>
2017-06-22 17:22:39 +0200

    - nginx.host property not needed anymore is now all handled via jenkins property which uses local_machine host name

--- java/testsuite/client/src/test/java/at/rpisec/testsuite/client/test/api/BaseIntegrationTest.java
+++ java/testsuite/client/src/test/java/at/rpisec/testsuite/client/test/api/BaseIntegrationTest.java
@@ -23,9 +23,13 @@ import java.util.Objects;
  */
 public class BaseIntegrationTest {
 
-    //protected static final String AUTH_BASE = "http://localhost:9080/rpisec-auth";
-	protected static final String AUTH_BASE = "http://" + System.getProperty("nginx.host", "localhost") + ":9080/rpisec-auth";
-	
+    private static final boolean jenkinsTest = Boolean.valueOf(System.getProperty("jenkins", "false"));
+    protected static final String AUTH_BASE;
+
+    static {
+        AUTH_BASE = "http://" + (jenkinsTest ? "local_machine" : "localhost") + ":9080/rpisec-auth";
+    }
+
     protected static final String AUTH_REST_SYSTEM_API_BASE = AUTH_BASE + "/test";
 
     @Before
@@ -65,7 +69,7 @@ public class BaseIntegrationTest {
      * @return the prepared rest template
      */
     private RestTemplate prepareRestTemplate(final String username,
-                                               final String password) {
+                                             final String password) {
         final RestTemplate template = new RestTemplate();
         if ((username != null) && (password != null)) {
             final String auth = username + ":" + password;
